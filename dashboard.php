de51b90bec32beda66a25181c148a8efca6670f5
Joe Carlson <jcarlson@boostability.com>
2014-07-29 07:54:02 -0600

    Changed around look of dashboard a little.

--- dashboard.php
+++ dashboard.php
@@ -36,78 +36,14 @@ $num = mysqli_num_rows($number);
 		<div style="min-width: 540px;" class="well well-sm"><span class="glyphicon glyphicon-link quickLink">&nbsp;</span><a href="suggestion.php">Support Tickets <span style="vertical-align: 1;" class="badge"><?php echo $num ?></span></a><a class="quickLink" href="admin.php">Administration</a></div>
 	<?php } ?>
 
-	<div style="text-align: center; background-color: white;" class="jumbotron">
+	<div class="left-wrapper-dash">
+
+	<div style="text-align: center;" class="jumbotron">
 		  <h1>Welcome to Connect</h1>
 		  <p>Boost Connect houses applications like AutoBuilder and Boost Note. <br> Click below to read documentation on these apps.</p>
 		  <p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
 	</div>
 
-	<div class="left-wrapper-dash">
-	
-	<?php if ($user_role == 'admin') { ?>
-	asdf
-
-	<?php } ?>
-
-	<?php if ($user_role == 'content') { ?>
-	<form class="panel taskform" autocomplete="off"  method="get" action="contentbuilder.php" >
-			<div class="panel-default">
-				<div class="panel-heading">
-					<div class="panel-title">
-					Domain Information <span class="sub-panel-title">- Set Domain & Wireframe </span>
-					</div>
-				</div>
-			</div>
-				<label for="domain_url">Domain URL:</label>
-				<input type="text" id="domain" name="domain" class="input-standard contentForm validation" placeholder="example.com" required />
-
-				<label for="wireframe_number">Wireframe Number:</label>
-				<input type="text" id="wireframe" name="wireframe" class="input-standard contentForm validation" placeholder="Wireframe Number" required />
-				<label for="language">Language:</label>
-				<select name="language" style="height: 33px;" class="input-standard contentForm validation" id="language">
-					<option value="english">English</option>
-					<option value="spanish">Spanish</option>
-					<option value="french">French</option>
-				</select>	
-				<input style="margin: 20px;" href="contentbuilder.php" class="submitButton btn-success" type="submit" />
-		</form>'
-	<?php } ?>
-
-	<?php if ($user_role == 'design') { ?>
-		<form class="panel taskform" autocomplete="off"  method="get" action="designbuilder.php" >	
-			<div class="panel-default">
-				<div class="panel-heading">
-					<div class="panel-title">
-					Domain Information <span class="sub-panel-title">- Set Domain & Wireframe </span>
-					</div>
-				</div>
-			</div>
-			
-				<label for="domain_url">Domain URL:</label>
-				<input type="text" id="domain" name="domain" class="input-standard contentForm" placeholder="example.com" required />
-
-				<label for="wireframe_number">Wireframe Number:</label>
-				<input type="text" id="wireframe" name="wireframe" class="input-standard contentForm" placeholder="Wireframe Number" required />
-				<input style="margin: 20px;" class="submitButton btn-success" type="submit" />
-		</form>'
-	<?php } ?>
-
-	<?php if ($user_role == 'blogs') { ?>
-		<form class="panel taskform" autocomplete="off"  method="get" action="devbuilder.php" >
-				
-			<div class="panel-default">
-				<div class="panel-heading">
-					<div class="panel-title">
-					Domain Information <span class="sub-panel-title">- Set Domain to Download</span>
-					</div>
-				</div>
-			</div>
-			
-			<label for="domain_url">Domain URL:</label>
-			<input type="text" id="domain" name="domain" class="input-standard contentForm" placeholder="example.com" required />
-			<input style="margin: 20px;" class="submitButton btn-success" type="submit" value="Download" />
-		</form>'
-	<?php } ?>
 
 	</div>
 	<div class="right-wrapper dashWrapper">
