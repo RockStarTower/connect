755a97c835044fff8241c3ffb00011d71353e650
Joe Carlson <jcarlson@boostability.com>
2014-08-05 14:18:10 -0600

    no message

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <link rel="stylesheet" type="text/css" href="style/style.css">
+<link rel="stylesheet" type="text/css" href="style/library.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
 <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
@@ -7,6 +8,7 @@
 <script type="text/javascript" src="js.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Tauri' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 <div  class="loginCon">
 	<div class="imgCon">
 	<img style="margin-left: 50px;" id="loginLogo" src="images/connectM.png">
