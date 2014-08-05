27f78615edf786896d46bd0856ce24838ae165df
boost-web <boost-web@users.noreply.github.com>
2014-08-05 14:16:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 <script type="text/javascript" src="js.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Tauri' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-
 <div  class="loginCon">
 	<div class="imgCon">
 	<img style="margin-left: 50px;" id="loginLogo" src="images/connectM.png">
